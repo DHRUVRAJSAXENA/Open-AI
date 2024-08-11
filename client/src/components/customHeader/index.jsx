930068196c8fccd9897d4e3a197183f3c234b413
DHRUVRAJSAXENA <dhruvrajsaxena1234@gmail.com>
2024-08-11 13:03:16 +0530

    first commit

---
+++ client/src/components/customHeader/index.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+import { PhoneIcon, ChatBubbleLeftRightIcon } from "@heroicons/react/24/solid"
+
+const CustomHeader = () => {
+  return <div className="chat-header">CustomHeader</div>
+}
+
+export default CustomHeader
